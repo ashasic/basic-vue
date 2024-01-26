7351d484296cc04638100048889ca8a006ca4c70
Juan Pablo Hourcade <juanpablo-hourcade@uiowa.edu>
2024-01-26 14:25:21 -0600

    Added Vue and Bootstrap files

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+
+    <div class="row">
+      <div class="col">
+        <h1>Sample Vue App with Bootstrap</h1>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col">
+        <p>This is a paragraph</p>
+      </div>
+    </div>
+  </div>
+</template>
+
